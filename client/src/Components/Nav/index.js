5ee2da0c5e464e89331e9288addd338fe1c41e53
Sara Bolduc <sara.s.bolduc@gmail.com>
2020-08-21 10:56:42 -0700

    adds navbar

---
+++ client/src/Components/Nav/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./style.css";
+
+function Nav() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              About
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Work
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+export default Nav;
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,13 +1,15 @@
 import React from "react";
-import Work from "../../Components/Work";
 import Carousel from "../../Components/Carousel";
+import Nav from "../../Components/Nav";
 import About from "../../Components/About";
+import Work from "../../Components/Work";
 import Contact from "../../Components/Contact";
 
 const Home = () => {
   return (
     <>
       <Carousel />
+      <Nav />
       <About />
       <Work />
       <Contact />
