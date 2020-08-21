3a557fb7297c602d15f959fa9622ed1e07266257
Sara Bolduc <sara.s.bolduc@gmail.com>
2020-08-21 11:35:17 -0700

    adds content to about section

--- client/src/Components/About/index.js
+++ client/src/Components/About/index.js
@@ -5,10 +5,23 @@ function About() {
   return (
     <div className="jumbotron jumbotron-fluid">
       <div className="container">
-        <h1 className="display-3">About</h1>
+        <img
+          src="assets/sb.jpg"
+          className="img-fluid selfie"
+          alt="sara bolduc"
+        />
         <p className="lead">
-          This is a modified jumbotron that occupies the entire horizontal space
-          of its parent.
+          <strong>Full Stack Developer</strong> <br />
+          <span className="sub">
+            with experience in Maker Education, media, and film
+          </span>
+          <br />
+          <br />
+          Using art & technology as mediums, I have designed learning
+          experiences and social events aimed to inspire curiosity & spark
+          imagination for over 2,000 students & families. I want to build
+          applications that have a social impact & foster community and
+          collaboration.
         </p>
       </div>
     </div>
