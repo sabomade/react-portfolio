abac88c4c871dec7258c94154f35dd7c5375ad6d
Sara Bolduc <sara.s.bolduc@gmail.com>
2020-10-24 18:29:07 -0700

    updates font style in Work component

--- client/src/Components/Work/index.js
+++ client/src/Components/Work/index.js
@@ -15,7 +15,7 @@ function Work() {
                 alt="web development"
               />
               <div class="card-body">
-                <h4 class="card-title">Web Development</h4>
+                <h4 class="card-title work-title">Web Development</h4>
                 <p class="card-text">
                   Full stack site design, development, & deployment
                 </p>
@@ -33,7 +33,7 @@ function Work() {
                 alt="maker education"
               />
               <div class="card-body">
-                <h4 class="card-title">Maker Education</h4>
+                <h4 class="card-title work-title">Maker Education</h4>
                 <p class="card-text">
                   STEAM-based curriculum design & facilitation for grades k-12
                 </p>
@@ -51,7 +51,7 @@ function Work() {
                 alt="product development"
               />
               <div class="card-body">
-                <h4 class="card-title">Product Development</h4>
+                <h4 class="card-title work-title">Product Development</h4>
                 <p class="card-text">STEAM-based kit design for ages 10+</p>
                 <a href="#!" class="">
                   Learn More
